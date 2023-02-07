2092d1a1cf1da8b9e7c5cd0149af3d48b790b293
Alyjah-Luna <alyjahluna@gmail.com>
2023-02-06 22:17:28 -0500

    created a model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Liquor(models.Model):
+    name = models.CharField(max_length=50)
+    spirit = models.CharField(max_length=50)
+    description = models.TextField(max_length=100)
+    ABV = models.IntegerField()
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
+liquors = [
+    {'name': 'Leyenda', 'spirit': 'Rum', 'description': '', 'ABV': '38%'},
+    {'name': 'Los Vecinos', 'spirit': 'Mezcal', 'description': '', 'ABV': '45%'}
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def liquors_index(request):
+    return render(request, 'liquors/index.html', {
+        'liquors': liquors
+    })
